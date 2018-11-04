e63948a38f3f3dd9beac13060da1c55455dff984
eterlan <703246769@qq.com>
2018-11-04 17:34:34 +0800

    transform move

---
+++ Assets/Scripts/gameplay/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// 
+/// </summary>
+public class Paddle:MonoBehaviour {
+	Rigidbody2D rb2d;
+	float velocityOfPeddle;	
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		velocityOfPeddle =  ConfigurationUtils.PaddleMoveUnitsPerSecond;
+	}
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis("Horizontal");
+		transform.Translate(new Vector3(velocityOfPeddle * horizontal,0,0));
+	}	
+}
